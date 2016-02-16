ccaceca7600d6c75ecf8e1f4dbdc1f4068ab6cda
Henry FitzGerald <hfitzgerald@seabreeze.ie>
2016-02-16 14:56:14 +0000

    11 answers

---
+++ 01-elements-selection/answers.js
@@ -0,0 +1,11 @@
+$("div:last");    				//last div
+$("img:last");    				//last img
+$("img").attr("alt","");		//images with alt attribute
+$("a");							//all anchor tags
+$("img");						//all image tags
+$(":header");					//all header elements
+$("#my_footer");				//footer by id
+$("#my_footer p");				//all p's in footer
+$("#my_footer > p");			//all direct p children in footer
+$(".card_image");				//by class
+$(":first");					//first element
