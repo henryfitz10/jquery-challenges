2e794379bc1f1be6e428ea128512f86afcaac29e
Henry FitzGerald <hfitzgerald@seabreeze.ie>
2016-02-16 16:27:26 +0000

    3 answers

---
+++ 03-adding-to-sites/answers.js
@@ -0,0 +1,3 @@
+$("p").append("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");		//append text
+$("a").remove();																//remove all links
+$(".card").empty();																//empty all cards
