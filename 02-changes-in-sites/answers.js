f189d6f9198cd61298f56dc18cf6ba43e2387514
Henry FitzGerald <hfitzgerald@seabreeze.ie>
2016-02-16 16:14:35 +0000

    6 answers

---
+++ 02-changes-in-sites/answers.js
@@ -0,0 +1,6 @@
+$("p").css("font-family");						//query paragraph font fam
+$("p").text("www.eir.ie");						//change content
+$("h2").text("Henry");							//change text, as above
+$("body").css("background-color","red");		//change backround of wiki page
+$("p").css("background-color","red");			//extra: p
+$("div").css("background-color","red");			//see above
